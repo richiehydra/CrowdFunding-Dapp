7de85f2d7f01c7d42660762d183dc9d8f3891633
Keshav Poojary <110924969+richiehydra@users.noreply.github.com>
2023-02-02 15:24:37 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,8 +7,8 @@ module.exports = {
   {
     goerli:
     {
-      url:"https://eth-goerli.g.alchemy.com/v2/uuJLpmgVtN-yGIqyQWEfJQbmSFvV-581",
-      accounts:["8a4f69299d51bc57c82246e00f914eea83b4719de46c56e3bcbb3aef6d5251f4"]
+      url:"Alchemy API KEY",
+      accounts:["PRIVATKEY"]
     },
   },
 };
